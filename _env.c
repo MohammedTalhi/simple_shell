5c99bb66caa08638444966179bf437d419798692
MohammedTalhi <talhi.mohammed@gmail.com>
2023-05-24 16:25:47 -0700

    another commit

--- _env.c
+++ _env.c
@@ -6,74 +6,68 @@
  * @var_value: The value to set for the variable.
  * Return: 0 if the operation is successful, otherwise -1.
  */
-
 int _setenv(char *var_name, char *var_value)
 {
-    int i = 0;
-    size_t name_len;
-    char *var_new;
+	int i = 0;
+	size_t name_len;
+	char *var_new;
 
-    name_len = _strlen(var_name);
-    i = 0;
+	name_len = _strlen(var_name);
 
-    // Updating an existing variable
-    while (environ[i])
-    {
-        if (strncmp(environ[i], var_name, name_len) == 0)
-        {
-            var_new = var_build(var_name, var_value);
+	/* Updating an existing variable */
+	while (environ[i])
+	{
+		if (_strncmp(environ[i], var_name, name_len) == 0)
+		{
+			var_new = _var_build(var_name, var_value);
 
-            // Clearing the memory before writing
-            environ[i] = NULL;
+			/* Clearing the memory before writing */
+			free(environ[i]);
 
-            // Updating the environment variable
-            environ[i] = _strdup(var_new);
-            free(environ[i]);
-            environ[i] = _strdup(var_new);
-            free(var_new);
-            return (0);
-        }
-        i++;
-    }
+			/* Updating the environment variable */
+			environ[i] = _strdup(var_new);
+			free(var_new);
+			return (0);
+		}
+		i++;
+	}
 
-    // Adding a variable that never existed before
-    var_new = var_build(var_name, var_value);
-    free(environ[i]);
-    environ[i] = _strdup(var_new);
-    i++;
-    environ[i] = NULL;
-    var_new = NULL;
+	/* Adding a variable that never existed before */
+	var_new = _var_build(var_name, var_value);
+	environ[i] = _strdup(var_new);
+	i++;
+	environ[i] = NULL;
+	free(var_new);
 
-    return (0);
+	return (0);
 }
 
 /**
- * var_build - Builds an environment variable string from its name and value.
+ * _var_build - Builds a new environment variable string.
  * @var_name: The name of the variable.
  * @var_value: The value of the variable.
- * Return: A string containing the full environment variable.
+ * Return: The new environment variable string, or NULL on failure.
  */
-
-char *var_build(char *var_name, char *var_value)
+char *_var_build(char *var_name, char *var_value)
 {
-    char *new_var;
-    size_t var_len;
+	char *new_var;
+	size_t var_len;
 
-    var_len = _strlen(var_name) + _strlen(var_value) + 2;
-    new_var = malloc(sizeof(char) * var_len);
-    if (new_var == NULL)
-    {
-        perror("Error: Insufficient memory\n");
-        return (NULL);
-    }
-    memset(new_var, 0, var_len);
+	var_len = _strlen(var_name) + _strlen(var_value) + 2;
+	new_var = malloc(sizeof(char) * var_len);
+	if (new_var == NULL)
+	{
+		perror("Error: Insufficient memory\n");
+		return (NULL);
+	}
+	memset(new_var, 0, var_len);
 
-    // Data in the form: var_name=var_value
-    new_var = _strcat(new_var, var_name);
-    new_var = _strcat(new_var, "=");
-    new_var = _strcat(new_var, var_value);
+	/* Data in the form: var_name=var_value */
+	_strcat(new_var, var_name);
+	_strcat(new_var, "=");
+	_strcat(new_var, var_value);
 
-    return (new_var);
+	return (new_var);
 }
 
 /**
@@ -83,29 +77,29 @@ char *var_build(char *var_name, char *var_value)
  */
 int _unsetenv(char *var_name)
 {
-    int i = 0;
-    char **env_temp;
-    size_t name_len;
+	int i = 0;
+	char **env_temp;
+	size_t name_len;
 
-    name_len = _strlen(var_name);
+	name_len = _strlen(var_name);
 
-    while (environ[i])
-    {
-        if (strncmp(environ[i], var_name, name_len) == 0)
-        {
-            env_temp = environ;
+	while (environ[i])
+	{
+		if (_strncmp(environ[i], var_name, name_len) == 0)
+		{
+			env_temp = environ;
 
-            // Freeing the memory of the environment variable
-            free(env_temp[0]);
+			/* Freeing the memory of the environment variable */
+			free(env_temp[0]);
 
-            do {
-                env_temp[0] = env_temp[1];
-                env_temp++;
-            } while (*env_temp);
-        }
-        i++;
-    }
+			do {
+				env_temp[0] = env_temp[1];
+				env_temp++;
+			} while (*env_temp);
+		}
+		i++;
+	}
 
-    return (0);
+	return (0);
 }
 
--- main.h
+++ main.h
@@ -1,61 +1,268 @@
-/***************************************************
- * main.h
- *
- * Header file for the main functions and utilities
- * used in the Simple Shell program.
- ***************************************************/
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <wait.h>
 #include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-
 #include <dirent.h>
 #include <signal.h>
 
-/* External variable to access the environment */
 extern char **environ;
 
-/* Function declarations */
+void _puts(char *str);
+void prompt(void);
+char *read_line(void);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char **_strtotokens(char *str);
+int _execute(char **tokens, char *args);
+void _printenv(void);
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+int _executeBuiltIn(char **tokens);
+int _isBuiltIn(char *str);
+int c_atoi(char *s);
+void _kill(char *lineptr, char *tmp, char **tok);
+void _exitSimpleShell(char **tokens, char *line);
+void ctrlc(int signum);
+int is_delim(char c, const char *delim);
+char *_strtok(char *src, const char *delim);
+char *_strcpy(char *dest, char *src);
+ssize_t get_line(char **str);
+int handle_path(char **tokens);
+int if_file_exists(char *s);
+char *path_builder(char **tokens);
+int execute2(char **tokens, char *path, char *args);
+char *var_build(char *var_name, char *var_value);
+int _setenv(char *var_name, char *var_value);
+int _unsetenv(char *var_name);
+char *_strdup(char *str);
 
-void _puts(char *str); // Function to print a string to the standard output
-void prompt(void); // Function to display the shell prompt
-char *read_line(void); // Function to read a line of input from the user
-int _strlen(char *s); // Function to calculate the length of a string
-int _strcmp(char *s1, char *s2); // Function to compare two strings
-int _executeBuiltIn(char **tokens); // Function to execute built-in shell commands
-int _isBuiltIn(char *str); // Function to check if a command is a built-in shell command
-int c_atoi(char *s); // Function to convert a string to an integer
-void _kill(char *lineptr, char *tmp, char **tok); // Function to handle the "kill" command
-void _exitSimpleShell(char **tokens, char *line); // Function to handle the "exit" command
-void ctrlc(int signum); // Signal handler for the Ctrl+C signal
-int is_delim(char c, const char *delim); // Function to check if a character is a delimiter
-char *_strtok(char *src, const char *delim); // Function to tokenize a string
-char *_strcpy(char *dest, char *src); // Function to copy a string
-char *_strcat(char *dest, char *src); // Function to concatenate two strings
-char **_strtotokens(char *str); // Function to convert a string to an array of tokens
-int _execute(char **tokens, char *args); // Function to execute a command
-void _printenv(void); // Function to print the environment variables
-int _putchar(char c); // Function to print a character to the standard output
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
-ssize_t get_line(char **str); // Function to read a line of input from a file
-int handle_path(char **tokens); // Function to handle the "path" command
-int if_file_exists(char *s); // Function to check if a file exists
-char *path_builder(char **tokens); // Function to build the full path of a command
-int execute2(char **tokens, char *path, char *args); // Function to execute a command with a specified path
-char *var_build(char *var_name, char *var_value); // Function to build an environment variable string
-int _setenv(char *var_name, char *var_value); // Function to set an environment variable
-int _unsetenv(char *var_name); // Function to unset an environment variable
-char *_strdup(char *str); // Function to duplicate a string
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
 
-#endif  /* MAIN_H */
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
 
+/* 1 if using system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+extern char **environ;
+
+
+/**
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - contains pseudo-arguements to pass into a function,
+ * allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguements
+ * @argv:an array of strings generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @histcount: the history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
+
+/**
+ * struct builtin - contains a builtin string and related function
+ * @type: the builtin command flag
+ * @func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+
+/* toem_shloop.c */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* toem_parser.c */
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/* loophsh.c */
+int loophsh(char **);
+
+/* toem_errors.c */
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+
+/* toem_string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* toem_string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* toem_exits.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/* toem_tokenizer.c */
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+
+/* toem_realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* toem_memory.c */
+int bfree(void **);
+
+/* toem_atoi.c */
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+/* toem_errors1.c */
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+
+/* toem_builtin.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* toem_builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+/*toem_getline.c */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+
+/* toem_getinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+
+/* toem_environ.c */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/* toem_getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/* toem_history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/* toem_lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* toem_lists1.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+/* toem_vars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
+
+#endif
 
